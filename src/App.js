81d45f613c94b9e811f4396e3a50b2531f756c79
EddyShimwa <shedpray16@gmail.com>
2023-06-15 13:17:41 +0200

    render audio source in app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import { songsdata } from './audios';
 
 const App = () => {
+   const [songs, setSongs] = useState(songsdata);
+   const [isplaying, setIsplaying] = useState(false);
+   const [currentSong, setCurrentSong] = useState(songsdata[0]);
+
     return <div className="App">
+        <audio src="https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse " />
         <Player />
     </div>;
 }
