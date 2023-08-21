748fd3d42bd67a1f56fcf2826e788fe7c0079093
EddyShimwa <shedpray16@gmail.com>
2023-08-21 22:45:32 +0200

    add the not found page

--- src/App.jsx
+++ src/App.jsx
@@ -72,14 +72,18 @@ return (
   </div>
   
   <div className="container mt-28 flex justify-center">
-  <div className="grid grid-cols-1 gap-4 w-80 ">
-    {albums.map((album, i) => (
+  <div className="grid grid-cols-1 gap-4 w-80">
+  {albums.length === 0 ? (
+    <p className="text-center text-red-600 font-semibold">Album not found</p>
+  ) : (
+    albums.map((album, i) => (
       <div className="bg-slate-200 rounded-lg shadow-md p-4" key={i}>
         <img src={album.images[0].url} alt={album.name} className="w-80 h-auto" />
         <h2 className="text-lg font-semibold mt-2 text-center text-gray-700">{album.name}</h2>
       </div>
-    ))}
-  </div>
+    ))
+  )}
+</div>
 </div>
 </div>
  );
